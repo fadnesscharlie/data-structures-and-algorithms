5b69cce7e39b99e66c9295342dfeec90cc846ebd
Charlie Fadness <boy_916@hotmail.com>
2021-07-19 22:13:18 -0700

    finished code challenge 3

--- javascript/code-challenges/challenges-02.test.js
+++ javascript/code-challenges/challenges-02.test.js
@@ -99,7 +99,7 @@ For example: charCode(['h','i']) returns [104, 105].
 
 const charCode = (arr) => {
   // Solution code here...
-  let mappedIndex = arr.map(letter => letter.charCodeAt(0));
+  let mappedIndex = arr.map(letter => letter.charCodeAt());
   return mappedIndex;
 };
 
@@ -225,27 +225,51 @@ const snorlaxStats = {
 const extractStats = (arr) => {
   // Solution code here...
   let nameAndStats = [];
-  // let objNameAndStats = new Object();
-  let objNameAndStatsName;
-  let objNameAndStatsTotal;
+  let objNameAndStats = new Object();
+  // let objNameAndStatsName;
+  // let objNameAndStatsTotal;
   arr.map((snorName, idx) => {
 
     let effort = +snorlaxStats.stats[idx].effort;
     let baseStat = +snorlaxStats.stats[idx].baseStat;
-    objNameAndStatsName = (snorlaxStats.stats[idx].stat.name);
-    objNameAndStatsTotal = effort + baseStat;
+    // objNameAndStatsName = (snorlaxStats.stats[idx].stat.name);
+    // objNameAndStatsTotal = effort + baseStat;
 
-    // objNameAndStats.name = (snorlaxStats.stats[idx].stat.name);
-    // objNameAndStats.total = effort + baseStat;
+    objNameAndStats.name = (snorlaxStats.stats[idx].stat.name);
+    objNameAndStats.total = effort + baseStat;
+    function hi() {
+      nameAndStats.map(item => {
+        nameAndStats[item].name, nameAndStats[item].total;
+        return nameAndStats;
+      });
+    }
+    hi();
   });
-  let final = {
-    name: objNameAndStatsName,
-    total: objNameAndStatsTotal,
-  };
-  nameAndStats.push(final);
+  nameAndStats.push(objNameAndStats);
+
+  var result = {};
+  for (var i = 0; i < nameAndStats.length; i++) {
+    result[nameAndStats[i].name] = nameAndStats[i].total;
+  }
+
+
+  // console.log(nameAndStats);
+
+  // let final = {
+  //   name: objNameAndStatsName,
+  //   total: objNameAndStatsTotal,
+  // };
+  // nameAndStats.push(final);
+
+  // const extractStats = objNameAndStats=> objNameAndStats.map(item => objNameAndStats[item].name, objNameAndStats[item].total);
 
   return nameAndStats;
+  // return result;
 };
+
+const secondTry = (arr) => arr.map(item => ({
+  name: item.stat.name, total: item.effort + item.baseStat
+}));
 
 /* ------------------------------------------------------------------------------------------------
 TESTS
@@ -311,29 +335,29 @@ describe('Testing challenge 6', () => {
 
 describe('Testing challenge 7', () => {
   test('It should return an array containing the character code for each letter', () => {
-    expect(charCode(['C', 'o', 'd', 'e', '3', '0', '1'])).toStrictEqual([ 67, 111, 100, 101, 51, 48, 49 ]);
+    expect(charCode(['C', 'o', 'd', 'e', '3', '0', '1'])).toStrictEqual([67, 111, 100, 101, 51, 48, 49]);
     expect(charCode(['C', 'o', 'd', 'e', '3', '0', '1']).length).toStrictEqual(7);
   });
 });
 
 describe('Testing challenge 8', () => {
   test('It should return an array containing the keys from an object', () => {
-    expect(evenOdd([5, 8, 2, 6, 9, 13, 542, 541])).toStrictEqual([ 'odd', 'even', 'even', 'even', 'odd', 'odd', 'even', 'odd' ]);
+    expect(evenOdd([5, 8, 2, 6, 9, 13, 542, 541])).toStrictEqual(['odd', 'even', 'even', 'even', 'odd', 'odd', 'even', 'odd']);
     expect(evenOdd([5, 8, 2, 6, 9, 13, 542, 541]).length).toStrictEqual(8);
   });
 
   test('It should work with all odd numbers', () => {
-    expect(evenOdd([1, 3, 5, 7, 9])).toStrictEqual([ 'odd', 'odd', 'odd', 'odd', 'odd' ]);
+    expect(evenOdd([1, 3, 5, 7, 9])).toStrictEqual(['odd', 'odd', 'odd', 'odd', 'odd']);
     expect(evenOdd([1, 3, 5, 7, 9]).length).toStrictEqual(5);
   });
 
   test('It should work with all even numbers', () => {
-    expect(evenOdd([2, 4, 6, 8, 10])).toStrictEqual([ 'even', 'even', 'even', 'even', 'even' ]);
+    expect(evenOdd([2, 4, 6, 8, 10])).toStrictEqual(['even', 'even', 'even', 'even', 'even']);
     expect(evenOdd([2, 4, 6, 8, 10]).length).toStrictEqual(5);
   });
 
   test('It should return the string "N/A" if a non-number is included in the array', () => {
-    expect(evenOdd([5, 8, 2, 'hi'])).toStrictEqual([ 'odd', 'even', 'even', 'N/A' ]);
+    expect(evenOdd([5, 8, 2, 'hi'])).toStrictEqual(['odd', 'even', 'even', 'N/A']);
     expect(evenOdd([5, 8, 2, 'hi']).length).toStrictEqual(4);
   });
 });
