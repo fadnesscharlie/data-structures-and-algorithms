11cb27e132e8eef80fb42f3fe84194cedf9bf3c9
Charlie <boy_916@hotmail.com>
2021-10-31 21:53:19 -0700

    not done

---
+++ code-challenge-401/code-challenge-09/README.md
@@ -0,0 +1,19 @@
+# 
+
+
+
+## Challenge
+
+
+
+## Approach & Efficiency
+
+
+
+## API
+
+There is no API's in the making of this challenge
+
+## Collaborators
+
+- []()
---
+++ code-challenge-401/code-challenge-09/stacks-and-queues/stack.js
@@ -0,0 +1,49 @@
+'use strict';
+
+class Node {
+  constructor() {
+    this.value = null;
+    this.next = null;
+  }
+}
+
+class Stack {
+  
+  constructor() {
+    this.storage = new Array();
+    this.top = null;
+  }
+
+  push(value) {
+    let node = new Node(value);
+    // unshift adds it to the beginning or top
+    this.storage.unshift(value); // Try not to use build in methods
+    this.top = value;
+  }
+
+  pop() {
+    // Temp varaible
+    // Reassign top
+    // reassign tempy.next to empty
+    // return
+    let temp = this.storage.shift(); // Try not to use build in methods
+    this.top = this.storage[0]
+    return temp
+  }
+
+  peek() {
+    // check if empty
+    if(this.isEmpty()) {
+      return;
+    }
+    console.log('Length: ', this.storage.length)
+    return this.top;
+  }
+
+  isEmpty() {
+
+  }
+
+}
+
+module.exports = Stack;
