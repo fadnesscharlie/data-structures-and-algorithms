3bb3c20e4478e3ef7715917f11a24e5950ba7eac
Charlie <boy_916@hotmail.com>
2022-01-11 15:57:12 -0800

    finished cc-30 hash map

--- code-challenge-401/cc-30/__tests__/hash.test.js
+++ code-challenge-401/cc-30/__tests__/hash.test.js
@@ -1,9 +1,26 @@
 'use strict';
 
+let Hashmap = require('../lib/hash.js');
+
+let myhash = new Hashmap(1024)
+myhash.set('John', 'Boss');
+myhash.set('Jonh', 'Boss2');
+
 describe('Hash', () => {
-  describe('True', () => {
-    it('should return true', () => {
-      expect(true).toBeTruthy();
+  describe('Get', () => {
+    it('Get should return results', () => {
+      let results = {
+        John: 'Boss'
+      }
+      expect(myhash.get('John')).toEqual(results)
+      expect(myhash.get('John1')).toBeFalsy();
+      
+    })
+  })
+  describe('Contains', () => {
+    it('should return true or false', () => {
+      expect(myhash.contains('john2')).toBeFalsy();
+      expect(myhash.contains('John')).toBeTruthy();
     })
   })
 })
